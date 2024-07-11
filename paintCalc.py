3ea40334daaa99ff6c8b9e1e83c6486377a5923a
Jason Zhang <jason789.zhang@gmail.com>
2024-07-11 15:07:34 -0600

    added skeleton code

---
+++ paintCalc.py
@@ -0,0 +1,30 @@
+def computeRoomArea(roomCount: int):
+    pass
+
+def computeRectangleWallsArea():
+    pass
+
+def computeRectangleArea(length, width):
+    pass
+
+def computeSquareWallsArea():
+    pass
+
+def computeSquareArea(sideLength):
+    pass
+
+def computeWindowsDoorsArea():
+    pass
+
+def computeCustomWallsArea():
+    pass
+
+def computeGallons(area):
+    pass
+
+def computePaintPrice(area):
+    pass
+
+
+if __name__ == "__main__":
+    pass
