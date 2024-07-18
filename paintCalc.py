76004ce1361bd1ebf9e28537b660e54f79d5fe79
Jason Zhang <jason789.zhang@gmail.com>
2024-07-18 15:53:37 -0600

    implemented computeRoomArea() as an recursive function

--- paintCalc.py
+++ paintCalc.py
@@ -1,5 +1,6 @@
 SQFT_PER_GALLON = 350
 COST_PER_GALLON = 42
+SHAPE = {"1":"rectangular", "2":"square", "3":"custom"}
 
 def computeRectangleWallsArea():
     length = float(input('input a the length of the rectangle wall in feet:\n')) #user to enter length
@@ -35,9 +36,44 @@ def computePaintPrice(area) -> float:
 # Basically the main function, most inputs are asked for here, 
 # and picking and choosing which functions are used
 def computeRoomArea(roomCount: int):
-    pass
+    if roomCount == 0:
+        return 0, 0, 0
+    else:
+        computeRoomArea.count += 1
+    
+    print(f"Room: {computeRoomArea.count}")
+    shapeSelection = input("Select the shape of the room:\n1 - Rectangular\n2 - Square\n3 - Custom (more or less than 4 walls, all square or rectangles)\n")
+    roomShape = SHAPE[shapeSelection]
+    
+    wallsArea = 0
+    if roomShape == "rectangular":
+        wallsArea = computeRectangleWallsArea()
+    elif roomShape == "square":
+        wallsArea = computeSquareWallsArea()
+    elif roomShape == "custom":
+        wallsArea = computeCustomWallsArea()
+
+    windowsDoorsArea = computeWindowsDoorsArea()
+    paintArea = wallsArea - windowsDoorsArea
+    paintVolume = paintArea / SQFT_PER_GALLON
+    paintCost = paintVolume * COST_PER_GALLON
+
+    print(f"\nFor Room: {computeRoomArea.count}, the area to be painted is {paintArea} square ft and will require {paintVolume:.2f} gallons to paint. This will cost the customer ${paintCost:.2f}\n")
+
+    recursiveTotal = computeRoomArea(roomCount-1)
+    
+    totalPaintArea = paintArea + recursiveTotal[0]
+    totalPaintVolume = paintVolume + recursiveTotal[1]
+    totalPaintCost = paintCost + recursiveTotal[2]
+
+    if computeRoomArea.count == roomCount:
+        print(f"Area to be painted is {totalPaintArea} square ft and will require {totalPaintVolume:.2f} gallons to paint. This will cost the customer ${totalPaintCost:.2f}")
+
+    return totalPaintArea, totalPaintVolume, totalPaintCost
+    
 
 if __name__ == "__main__":
-    roomCount = int(input("Welcome to Shiny Paint Company for indoor painting!\nHow many Rooms do you want to paint:"))
+    roomCount = int(input("Welcome to Shiny Paint Company for indoor painting!\nHow many Rooms do you want to paint:\n"))
     print("Thank you!")
+    computeRoomArea.count = 0
     computeRoomArea(roomCount)
